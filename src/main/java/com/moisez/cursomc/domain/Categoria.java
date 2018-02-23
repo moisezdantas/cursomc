1e96a198bdd74cdc56dbe00a29319fb8e0084039
Moisez Dantas <moises1695@gmail.com>
2018-02-22 21:15:11 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/moisez/cursomc/domain/Categoria.java
+++ src/main/java/com/moisez/cursomc/domain/Categoria.java
@@ -5,15 +5,25 @@ package com.moisez.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * @author moisez_dantas
  *
- */
+ */ 
+@Entity
 public class Categoria implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	
+	
 	private String nome;
 	
 	public Categoria() {
